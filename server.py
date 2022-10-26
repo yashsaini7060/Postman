feabb38b2bce9757b35f36d777fa8dd9c2ee807b
yashsaini7060 <yk18052000@gmail.com>
2022-10-26 13:52:28 +0530

    first commit

---
+++ server.py
@@ -0,0 +1,17 @@
+import os
+import socket
+import sys
+
+
+# Visit https://edstem.org/au/courses/8961/lessons/26522/slides/196175 to get
+PERSONAL_ID = '09665A'
+PERSONAL_SECRET = '4c1ad1b77651992faa6e31e7f3cbdb8b'
+
+
+def main():
+    # TODO
+    pass
+
+
+if __name__ == '__main__':
+    main()
