4cd137be0ed541d38d7e7e4cd8c4df3ffbafe949
yashsaini7060 <yk18052000@gmail.com>
2022-10-27 01:38:37 +0530

    added references

--- client.py
+++ client.py
@@ -1,17 +1,45 @@
-import os
 import socket
-import sys
+
+IP = socket.gethostbyname(socket.gethostname())
+PORT = 5580
+ADDR = (IP, PORT)
+FORMAT = "utf-8"
+SIZE = 1024
 
 
-# Visit https://edstem.org/au/courses/8961/lessons/26522/slides/196175 to get
-PERSONAL_ID = '09665A'
-PERSONAL_SECRET = '4c1ad1b77651992faa6e31e7f3cbdb8b' 
+# PERSONAL_ID = '09665A'
+# PERSONAL_SECRET = '4c1ad1b77651992faa6e31e7f3cbdb8b' 
+
+
 
 
 def main():
-    # TODO
-    pass
+    """ Staring a TCP socket. """
+    client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+    """ Connecting to the server. """
+    client.connect(ADDR)
+
+    """ Opening and reading the file data. """
+    file = open("data/yt.txt", "r")
+    data = file.read()
+
+    """ Sending the filename to the server. """
+    client.send("yt.txt".encode(FORMAT))
+    msg = client.recv(SIZE).decode(FORMAT)
+    print(f"[SERVER]: {msg}")
+
+    """ Sending the file data to the server. """
+    client.send(data.encode(FORMAT))
+    msg = client.recv(SIZE).decode(FORMAT)
+    print(f"[SERVER]: {msg}")
+
+    """ Closing the file. """
+    file.close()
+
+    """ Closing the connection from the server. """
+    client.close()
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     main()
--- server.py
+++ server.py
@@ -1,17 +1,53 @@
-import os
 import socket
-import sys
+
+IP = socket.gethostbyname(socket.gethostname())
+PORT = 5580
+ADDR = (IP, PORT)
+SIZE = 1024
+FORMAT = "utf-8"
 
 
-# Visit https://edstem.org/au/courses/8961/lessons/26522/slides/196175 to get
-PERSONAL_ID = '09665A'
-PERSONAL_SECRET = '4c1ad1b77651992faa6e31e7f3cbdb8b'
+# PERSONAL_ID = '09665A'
+# PERSONAL_SECRET = '4c1ad1b77651992faa6e31e7f3cbdb8b' 
+
+
 
 
 def main():
-    # TODO
-    pass
+    print("[STARTING] Server is starting.")
+    """ Staring a TCP socket. """
+    server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
+    """ Bind the IP and PORT to the server. """
+    server.bind(ADDR)
 
-if __name__ == '__main__':
+    """ Server is listening, i.e., server is now waiting for the client to connected. """
+    server.listen()
+    print("[LISTENING] Server is listening.")
+
+    while True:
+        """ Server has accepted the connection from the client. """
+        conn, addr = server.accept()
+        print(f"[NEW CONNECTION] {addr} connected.")
+
+        """ Receiving the filename from the client. """
+        filename = conn.recv(SIZE).decode(FORMAT)
+        print(f"[RECV] Receiving the filename.")
+        file = open(filename, "w")
+        conn.send("Filename received.".encode(FORMAT))
+
+        """ Receiving the file data from the client. """
+        data = conn.recv(SIZE).decode(FORMAT)
+        print(f"[RECV] Receiving the file data.")
+        file.write(data)
+        conn.send("File data received".encode(FORMAT))
+
+        """ Closing the file. """
+        file.close()
+
+        """ Closing the connection from the client. """
+        conn.close()
+        print(f"[DISCONNECTED] {addr} disconnected.")
+
+if __name__ == "__main__":
     main()
