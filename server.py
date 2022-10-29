3340a8436ababcff91d7bfca993211ac977e0f15
yashsaini7060 <yk18052000@gmail.com>
2022-10-29 11:06:11 +0530

    added server

--- server.py
+++ server.py
@@ -32,50 +32,46 @@ def get_port_and_path():
 
 
 def main():
-    PORT=3444
-    INBOX_PATH="non"
     PORT, INBOX_PATH = get_port_and_path()
-    print(PORT)
-    print(INBOX_PATH)
-    # IP = socket.gethostbyname(socket.gethostname())
-    # ADDR = (IP, PORT)
-    # SIZE = 1024
-    # FORMAT = "utf-8"
+    IP = socket.gethostbyname(socket.gethostname())
+    ADDR = (IP, PORT)
+    SIZE = 1024
+    FORMAT = "utf-8"
 
     # print("[STARTING] Server is starting.")
-    # """ Staring a TCP socket. """
-    # server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    """ Staring a TCP socket. """
+    server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
-    # """ Bind the IP and PORT to the server. """
-    # server.bind(ADDR)
+    """ Bind the IP and PORT to the server. """
+    server.bind(ADDR)
 
-    # """ Server is listening, i.e., server is now waiting for the client to connected. """
-    # server.listen()
+    """ Server is listening, i.e., server is now waiting for the client to connected. """
+    server.listen()
     # print("[LISTENING] Server is listening.")
+    print("S: 220 Service ready")
+    while True:
+        """ Server has accepted the connection from the client. """
+        conn, addr = server.accept()
+        print(f"[NEW CONNECTION] {addr} connected.")
 
-    # while True:
-    #     """ Server has accepted the connection from the client. """
-    #     conn, addr = server.accept()
-    #     print(f"[NEW CONNECTION] {addr} connected.")
+        """ Receiving the filename from the client. """
+        filename = conn.recv(SIZE).decode(FORMAT)
+        print(f"[RECV] Receiving the filename.")
+        file = open(filename, "w")
+        conn.send("Filename received.".encode(FORMAT))
 
-    #     """ Receiving the filename from the client. """
-    #     filename = conn.recv(SIZE).decode(FORMAT)
-    #     print(f"[RECV] Receiving the filename.")
-    #     file = open(filename, "w")
-    #     conn.send("Filename received.".encode(FORMAT))
+        """ Receiving the file data from the client. """
+        data = conn.recv(SIZE).decode(FORMAT)
+        print(f"[RECV] Receiving the file data.")
+        file.write(data)
+        conn.send("File data received".encode(FORMAT))
 
-    #     """ Receiving the file data from the client. """
-    #     data = conn.recv(SIZE).decode(FORMAT)
-    #     print(f"[RECV] Receiving the file data.")
-    #     file.write(data)
-    #     conn.send("File data received".encode(FORMAT))
+        """ Closing the file. """
+        file.close()
 
-    #     """ Closing the file. """
-    #     file.close()
-
-    #     """ Closing the connection from the client. """
-    #     conn.close()
-    #     print(f"[DISCONNECTED] {addr} disconnected.")
+        """ Closing the connection from the client. """
+        conn.close()
+        print(f"[DISCONNECTED] {addr} disconnected.")
 
 if __name__ == "__main__":
     main()
