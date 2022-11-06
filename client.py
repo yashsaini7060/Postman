1226f42d2191b80fbe52f1df458058256385ff9f
yashsaini7060 <yk18052000@gmail.com>
2022-11-06 12:03:56 +0530

    solved err working upto ehol

--- client.py
+++ client.py
@@ -7,42 +7,14 @@ from datetime import datetime
 
 PERSONAL_ID = '09665A'
 PERSONAL_SECRET = '4c1ad1b77651992faa6e31e7f3cbdb8b' 
-IP = "127.0.0.1"
-
-
-@dataclass(frozen=True)
-class EmailUser:
-    """
-    A dataclass containing information about an email sender/recipient.
-    """
-    name: str
-    """The full name of the email sender/recipient."""
-
-    address: str
-    """The email address of the email sender/recipient."""
-
-
-@dataclass(frozen=True)
-class Email:
-    """
-    A dataclass containing all necessary information to send a plaint text email.
-    """
-    from_user: EmailUser
-    """The sender of the email."""
-
-    recipients: tuple[EmailUser, ...]
-    """All recipients of the email."""
-
-    subject: str
-    """The subject of the email (which may be empty)."""
-
-    body: str
-    """The body of the email as a string."""
-
-
-
-
-def get_configs():
+IP = '127.0.0.1'
+FORMAT = "utf-8"
+SIZE = 1024
+PORT=0
+INBOX_PATH=''
+def get_port_and_path():
+    global PORT
+    global INBOX_PATH
     try:
         config_file=sys.argv[1]
 
@@ -56,9 +28,11 @@ def get_configs():
         # SERVER PORT
 
         server_port=lines[0]
-        if server_port.lower().startswith("send_path"):
+        if server_port.lower().startswith("server_port"):
             x = server_port.split("=")
             server_port=x[1]
+            if server_port.endswith("\n"):
+                server_port=server_port[:-1]
             server_port=int(server_port)
             
             if server_port < 1024:
@@ -70,6 +44,8 @@ def get_configs():
         if client_port.lower().startswith("client_port"):
             x = client_port.split("=")
             client_port=x[1]
+            if client_port.endswith("\n"):
+                client_port=client_port[:-1]
             client_port=int(client_port)
 
             if client_port < 1024 and server_port==client_port:
@@ -120,76 +96,11 @@ def get_configs():
         else:
             exit(2)
         
-        return server_port, inbox_path
-
+        # return server_port, inbox_path
+        PORT=server_port
+        INBOX_PATH=inbox_path
     except:
         exit(1)
-
-def get_email_body() -> str:
-    """
-    Gets the content of the email body.
-    Returns:
-        The content of the email body as a string.
-    """
-    print("Enter email contents below (press Ctrl+D to finish):")
-    content = sys.stdin.read()
-
-    return content
-
-
-def get_to_users() -> tuple[EmailUser, ...]:
-    """
-    Gets all recipients that the email will be sent to.
-    Returns:
-        A tuple containing all recipients the email will be addressed to.
-    """
-    to_users = []
-    input_finished = False
-    while not input_finished:
-        to_name = input("Enter a recipient name: ")
-        to_addr = input("Enter this recipients address: ")
-        to_user = EmailUser(to_name, to_addr)
-        to_users.append(to_user)
-
-        input_users_confirmation = input("Would you like to input any more recipients? [y/N] ").lower()
-        if input_users_confirmation == "y":
-            continue
-        else:
-            input_finished = True
-        
-        print()
-    
-    print(f"{len(to_users)} total recipients entered")
-    return tuple(to_users)
-
-
-def get_from_user() -> EmailUser:
-    """
-    Gets information about the user sending the email.
-    Returns:
-        An EmailUser representing the user sending the email.
-    """
-    from_name = input("Enter the from name: ")
-    from_addr = input("Enter the first part of the from address (i.e. the <name> in "
-                      "<name>@it.usyd.edu.au): ") + "@it.usyd.edu.au"
-    from_user = EmailUser(from_name, from_addr)
-    return from_user
-
-
-def get_email_data() -> Email:
-    """
-    Gets all necessary information required to send an email via the USYD mail server via SMTP.
-    Returns:
-        An Email object containing all necessary information for a plain text email.
-    """
-    from_user = get_from_user()
-    to_users = get_to_users()
-    subject = input("Enter subject (press 'Enter' for no subject): ")
-    content = get_email_body()
-
-    email = Email(from_user, to_users, subject, content)
-    return email
-
 
 
 
@@ -202,26 +113,28 @@ def setup_client_connection() -> socket.socket:
     Returns:
         A client socket connected to the USYD mail server.
     """
-
-    PORT, INBOX_PATH = get_configs()
-
-    SMTP_SERVER = (IP, PORT)
-
+    global PORT
+    global IP
+    # PORT, INBOX_PATH = get_configs()
+    get_port_and_path()
+    # SMTP_SERVER = (IP, PORT)
+    # PORT=int(PORT)
+    # print(PORT)
+    # print(IP)
     sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     # allows us to relaunch the application quickly without having to worry about "address already 
     # in use errors"
     # sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
     # Sets timeout of socket: we will use this to check if we can connect to the server
-    sock.settimeout(20)
+    sock.settimeout(5)
 
     try:
         # Connct to server
-        sock.connect(SMTP_SERVER)
-    except TimeoutError:
-        print("exit_code 3")
-        exit(3)
         
-    print(IP)
+        sock.connect((IP,PORT))
+    except:
+        sys.exit("C: Cannot establish connection")
+
     return sock
 
 def check_status_code(client_sock: socket.socket, expected_status_code: int) -> None:
@@ -236,7 +149,7 @@ def check_status_code(client_sock: socket.socket, expected_status_code: int) -> 
         ValueError: if the status code returned by the server does not match expected_status_code.
     """
     # Get response from server
-    server_data = client_sock.recv(1024)
+    server_data = client_sock.recv(SIZE)
     server_data_str = server_data.decode()
     # Split response from server into list (on whitespace)
     server_data_ls = server_data_str.split()
@@ -251,54 +164,54 @@ def check_status_code(client_sock: socket.socket, expected_status_code: int) -> 
         print(out_str, flush=True)
         # print(f"S: {server_data_str}")
 
-def send_data(client_socket: socket.socket, email: Email) -> None:
-    """
-    Sends the DATA message to the mail server, as well as the MIME data portion of the email.
-    Args:
-        client_socket: the client socket connected to the USYD mail server.
-        email: the Email object containing all necessary information for a plain text email.
-    """
-    client_socket.send(b"DATA\r\n")
-    check_status_code(client_socket, 354)
+# def send_data(client_socket: socket.socket, email: Email) -> None:
+#     """
+#     Sends the DATA message to the mail server, as well as the MIME data portion of the email.
+#     Args:
+#         client_socket: the client socket connected to the USYD mail server.
+#         email: the Email object containing all necessary information for a plain text email.
+#     """
+#     client_socket.send(b"DATA\r\n")
+#     check_status_code(client_socket, 354)
 
-    mime_version = "MIME-Version: 1.0\r\n"
+#     mime_version = "MIME-Version: 1.0\r\n"
 
-    date = datetime.now().astimezone()
-    date_str = date.strftime("Date: %a, %-d %b %Y %X %z\r\n")
+#     date = datetime.now().astimezone()
+#     date_str = date.strftime("Date: %a, %-d %b %Y %X %z\r\n")
 
-    from_header = f"From: {email.from_user.name} <{email.from_user.address}>\r\n"
+#     from_header = f"From: {email.from_user.name} <{email.from_user.address}>\r\n"
 
-    to_users_str = [f"{recipient.name} <{recipient.address}>" for recipient in email.recipients]
-    to_header = "To: " + ", ".join(to_users_str) + "\r\n"
+#     to_users_str = [f"{recipient.name} <{recipient.address}>" for recipient in email.recipients]
+#     to_header = "To: " + ", ".join(to_users_str) + "\r\n"
 
-    subject = f"Subject: {email.subject}\r\n"
+#     subject = f"Subject: {email.subject}\r\n"
 
-    content_type = "Content-Type: text/plaintext;\r\n"
+#     content_type = "Content-Type: text/plaintext;\r\n"
 
-    client_socket.send(mime_version.encode())
-    client_socket.send(date_str.encode())
-    client_socket.send(from_header.encode())
-    client_socket.send(to_header.encode())
-    client_socket.send(subject.encode())
-    client_socket.send(content_type.encode())
-    client_socket.send(b"\r\n")
-    client_socket.send(email.body.encode() + b"\r\n")
+#     client_socket.send(mime_version.encode())
+#     client_socket.send(date_str.encode())
+#     client_socket.send(from_header.encode())
+#     client_socket.send(to_header.encode())
+#     client_socket.send(subject.encode())
+#     client_socket.send(content_type.encode())
+#     client_socket.send(b"\r\n")
+#     client_socket.send(email.body.encode() + b"\r\n")
 
-    client_socket.send(b".\r\n")
-    check_status_code(client_socket, 250)
+#     client_socket.send(b".\r\n")
+#     check_status_code(client_socket, 250)
 
 
-def send_to_recipients(client_socket: socket.socket, email: Email) -> None:
-    """
-    Send a RCPT TO messager to the mail server for all recipients
-    Args:
-        client_socket: the client socket connected to the USYD mail server.
-        email: the Email object containing all necessary information for a plain text email.
-    """
-    for recipient in email.recipients:
-        rcpt_to = f"RCPT TO:<{recipient.address}>\r\n"
-        client_socket.send(rcpt_to.encode())
-        check_status_code(client_socket, 250)
+# def send_to_recipients(client_socket: socket.socket, email: Email) -> None:
+#     """
+#     Send a RCPT TO messager to the mail server for all recipients
+#     Args:
+#         client_socket: the client socket connected to the USYD mail server.
+#         email: the Email object containing all necessary information for a plain text email.
+#     """
+#     for recipient in email.recipients:
+#         rcpt_to = f"RCPT TO:<{recipient.address}>\r\n"
+#         client_socket.send(rcpt_to.encode())
+#         check_status_code(client_socket, 250)
 
 
 def send_helo(client_sock: socket.socket) -> None:
@@ -307,59 +220,55 @@ def send_helo(client_sock: socket.socket) -> None:
     Args:
         client_sock: the client socket connected to the USYD mail server.
     """
-    ehol="EHOL " + IP +"\r\n"
-    client_sock.send(b"EHLO 127.0.0.1\r\n")
-    print("C: EHLO 127.0.0.1")
+    client_sock.send(b"HELO 127.0.0.1\r\n")
+    print("C: HELO 127.0.0.1")
 
 
-def send_email_via_server(client_sock: socket.socket, email: Email) -> None:
-    """
-    Communicates with the USYD mail server in order to send an email using the SMTP protocol.
-    Args:
-        client_sock: the client socket connected to the USYD mail server.
-        email: the Email object containing all necessary information for a plain text email.
-    """
-    with client_sock:
-        # Check initial status code after connection (220)
-        check_status_code(client_sock, 220)
+# def send_email_via_server(client_sock: socket.socket, email: Email) -> None:
+#     """
+#     Communicates with the USYD mail server in order to send an email using the SMTP protocol.
+#     Args:
+#         client_sock: the client socket connected to the USYD mail server.
+#         email: the Email object containing all necessary information for a plain text email.
+#     """
+#     with client_sock:
+#         # Check initial status code after connection (220)
+#         check_status_code(client_sock, 220)
+#         print
+#         # Send the HELO message, and check the status code
+#         # send_helo(client_sock)
+#         # check_status_code(client_sock, 250)
 
-        # Send the HELO message, and check the status code
-        send_helo(client_sock)
-        check_status_code(client_sock, 250)
+#         # Send the MAIL FROM message to the server, and check the status code
+#         from_user = email.from_user
+#         mail_from = f"MAIL FROM:<{from_user}>\r\n"
+#         client_sock.send(mail_from.encode())
+#         # check_status_code(client_sock, 250)
 
-        # Send the MAIL FROM message to the server, and check the status code
-        from_user = email.from_user
-        mail_from = f"MAIL FROM:<{from_user}>\r\n"
-        client_sock.send(mail_from.encode())
-        # check_status_code(client_sock, 250)
+#         # Send the RCPT TO headers for all recipients
+#         send_to_recipients(client_sock, email)
 
-        # Send the RCPT TO headers for all recipients
-        send_to_recipients(client_sock, email)
+#         # Send DATA, and email data to server
+#         send_data(client_sock, email)
 
-        # Send DATA, and email data to server
-        send_data(client_sock, email)
+#         # Quit server gracefully by sending QUIT
+#         client_sock.send(b"QUIT\r\n")
 
-        # Quit server gracefully by sending QUIT
-        client_sock.send(b"QUIT\r\n")
-
-        # Printed if no exceptions occur
-        print("Email sent successfully!")
+#         # Printed if no exceptions occur
+#         print("Email sent successfully!")
 
 
 
 
 
 def main():
-    try:
-        client_sock = setup_client_connection()
-        with client_sock:
-            check_status_code(client_sock, 220)
-            send_helo(client_sock)
-        # email = get_email_data()
-        # send_email_via_server(client_sock, email)
-    except:
-        print("C: Cannot establish connection")
-    
+    # print("S: 220 Service ready", flush=True)
+    client_sock = setup_client_connection()
+    with client_sock:
+        check_status_code(client_sock, 220)
+        send_helo(client_sock)
+    # email = get_email_data()
+    # send_email_via_server(client_sock, email)
  
 
 
--- server.py
+++ server.py
@@ -4,10 +4,14 @@ import os
 
 # PERSONAL_ID = '09665A'
 # PERSONAL_SECRET = '4c1ad1b77651992faa6e31e7f3cbdb8b' 
-
-IP = "127.0.0.1"
-
+IP = '127.0.0.1'
+FORMAT = "utf-8"
+SIZE = 1024
+PORT=0
+INBOX_PATH=''
 def get_port_and_path():
+    global PORT
+    global INBOX_PATH
     try:
         config_file=sys.argv[1]
 
@@ -21,9 +25,11 @@ def get_port_and_path():
         # SERVER PORT
 
         server_port=lines[0]
-        if server_port.lower().startswith("send_path"):
+        if server_port.lower().startswith("server_port"):
             x = server_port.split("=")
             server_port=x[1]
+            if server_port.endswith("\n"):
+                server_port=server_port[:-1]
             server_port=int(server_port)
             
             if server_port < 1024:
@@ -35,6 +41,8 @@ def get_port_and_path():
         if client_port.lower().startswith("client_port"):
             x = client_port.split("=")
             client_port=x[1]
+            if client_port.endswith("\n"):
+                client_port=client_port[:-1]
             client_port=int(client_port)
 
             if client_port < 1024 and server_port==client_port:
@@ -84,74 +92,65 @@ def get_port_and_path():
             pass
         else:
             exit(2)
-        
-        return server_port, inbox_path
-
+        # print(server_port)
+        # return server_port, inbox_path
+        PORT=server_port
+        INBOX_PATH=inbox_path
     except:
         exit(1)
 
-# def get_port_and_path():
-#     try:
-#         config_file=sys.argv[1]
-#         f = open(config_file,"r")
-#         lines = f.readlines()
-#         server_port=lines[0]
-#         x = server_port.split("=")
-#         server_port=x[1]
-#         server_port=int(server_port)
-#         inbox_path=lines[2]
-#         x = inbox_path.split("~")
-#         inbox_path=x[1]
-#         if inbox_path.endswith("\n"):
-#             inbox_path=inbox_path[:-1]
-#             inbox_path=inbox_path[1:]
-#         if os.access(inbox_path, os.W_OK):
-#             return server_port, inbox_path
-#         else:
-#             exit(2)
-#     except:
-#         exit(1)
 
-
-
+def send_data(conn , string):
+    conn.send(string.encode())
+    new_str="S: " + string
+    print(new_str)
+    client_response= conn.recv(SIZE).decode()
+    return client_response
 
 def main():
-    PORT, INBOX_PATH = get_port_and_path()
-
-    ADDR = (IP, PORT)
-    SIZE = 1024
-    FORMAT = "utf-8"
-
+    global PORT
+    global IP
+    get_port_and_path()
+    # print(PORT)
+    # print(IP)
+    # PORT, INBOX_PATH = 
+    # print(type(PORT))
+    # if PORT.endswith("\n"):
+    #     PORT=PORT[:-1]
+    # PORT=int(PORT)
     # print("[STARTING] Server is starting.")
     """ Staring a TCP socket. """
     server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
     """ Bind the IP and PORT to the server. """
-    server.bind(ADDR)
+    # print(PORT)
+    server.bind((IP, PORT))
 
     """ Server is listening, i.e., server is now waiting for the client to connected. """
-    server.listen()
+    server.listen(1)
 
     while True:
         """ Server has accepted the connection from the client. """
         conn, addr = server.accept()
-        conn.send("220 Service ready".encode(FORMAT))
 
+        # Service redy messagess
 
-        """ Receiving the hello  from the client. """
-        hello = conn.recv(SIZE).decode(FORMAT)
-        print(hello)
-        ehol
-        conn.send("250 Service ready".encode(FORMAT))
-        mail_from = conn.recv(SIZE).decode(FORMAT)
-        print(mail_from)
-        conn.send("250 Service ready".encode(FORMAT))
-        send_to_recipient = conn.recv(SIZE).decode(FORMAT)
-        print(send_to_recipient)
-        conn.send("250 Service ready".encode(FORMAT))
-        send_data = conn.recv(SIZE).decode(FORMAT)
-        print(send_data)
-        conn.send("250 Service ready".encode(FORMAT))
+        ehol_response = send_data(conn, "220 Service ready")
+        ehol_response = "C: " + ehol_response
+        print(ehol_response)
+
+        # filename = conn.recv(SIZE).decode(FORMAT)
+        # print(filename)
+        # conn.send("250 Service ready".encode(FORMAT))
+        # mail_from = conn.recv(SIZE).decode(FORMAT)
+        # print(mail_from)
+        # conn.send("250 Service ready".encode(FORMAT))
+        # send_to_recipient = conn.recv(SIZE).decode(FORMAT)
+        # print(send_to_recipient)
+        # conn.send("250 Service ready".encode(FORMAT))
+        # send_data = conn.recv(SIZE).decode(FORMAT)
+        # print(send_data)
+        # conn.send("250 Service ready".encode(FORMAT))
 
 
         """ Closing the connection from the client. """
@@ -159,4 +158,4 @@ def main():
         print(f"[DISCONNECTED] {addr} disconnected.")
 
 if __name__ == "__main__":
-    main()
+    main() 
