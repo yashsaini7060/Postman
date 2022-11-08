e0672af27a2867a716137f3b6f3cac79e5e48661
yashsaini7060 <yk18052000@gmail.com>
2022-11-08 17:23:17 +0530

    while loop

--- server.py
+++ server.py
@@ -87,17 +87,17 @@ def send_response(conn , string):
 def get_response(conn):
 
     client_response= conn.recv(SIZE).decode(FORMAT)
-    if not client_response:
-        print("S: Connection lost", flush=True)
-    else:
-        if client_response.endswith('\n'):
-            client_response=client_response[:-1]
-        out_str = "C: " + client_response
-        print(out_str, flush=True)
+    # if not client_response:
+    #     print("S: Connection lost", flush=True)
+    # else:
+    if client_response.endswith('\n'):
+        client_response=client_response[:-1]
+    out_str = "C: " + client_response
+    print(out_str, flush=True)
 
-        client_code=client_response.split(" ")
+    client_code=client_response.split(" ")
 
-        return client_code[0], client_response
+    return client_code[0], client_response
 
 
 
