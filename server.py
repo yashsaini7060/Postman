b18a4d4137503cb73255659e91cb52a8bd846962
yashsaini7060 <yk18052000@gmail.com>
2022-11-08 13:39:28 +0530

    client

--- server.py
+++ server.py
@@ -12,88 +12,57 @@ INBOX_PATH=''
 def get_port_and_path():
     global PORT
     global INBOX_PATH
+    config_file=''
     try:
+        config_file=sys.argv[1]
+    except:
+        exit(1)
+    if config_file!='':
         config_file=sys.argv[1]
 
         # FILE READINGG
         f = open(config_file,"r")
         lines = f.readlines()
-        if(len(lines)<5):
+        # print(lines)
+        if len(lines)<2:
             exit(2)
-
 
         # SERVER PORT
+        server_port=''
 
-        server_port=lines[0]
-        if server_port.lower().startswith("server_port"):
-            x = server_port.split("=")
-            server_port=x[1]
-            if server_port.endswith("\n"):
-                server_port=server_port[:-1]
-            server_port=int(server_port)
-            
+        for x in lines:
+            if x.lower().startswith("server_port"): 
+                server_port=x
+        
+        if server_port!='':
+            server_port = server_port.split('=')
+            server_port=server_port[1]
+            if server_port.endswith('\n'):
+                server_port=int(server_port[:-1])
             if server_port < 1024:
                 exit(2)
-
-        # CLIENT PORT
-
-        client_port=lines[1]
-        if client_port.lower().startswith("client_port"):
-            x = client_port.split("=")
-            client_port=x[1]
-            if client_port.endswith("\n"):
-                client_port=client_port[:-1]
-            client_port=int(client_port)
-
-            if client_port < 1024 and server_port==client_port:
-                exit(2)
-
-        
-        # INBOX PATH
-        inbox_path=lines[2]
-        
-        if inbox_path.lower().startswith("inbox_path"):
-            x = inbox_path.split("=")
-            inbox_path=x[1]
-            if inbox_path.endswith("\n"):
-                inbox_path=inbox_path[:-1]
-            if inbox_path.startswith("~/"):
-                inbox_path=inbox_path[2:]
-            if os.access(inbox_path, os.W_OK):
-                pass
-            else:
-                exit(2)
+        else:
+            exit(2)
 
         # SEND PATH
-        send_path=lines[3]
-        if send_path.lower().startswith("send_path"):
-            x = send_path.split("=")
-            send_path=x[1]
-        if send_path.endswith("\n"):
-            send_path=send_path[:-1]
-        if os.access(send_path, os.R_OK):
-            pass
+        send_path=''
+
+        for x in lines:
+            if x.lower().startswith("inbox_path"): 
+                x = x.split("=")
+                x=x[1]
+                if x.endswith('\n'):
+                    x=x[:-1]
+                send_path=x
+        if send_path!='':
+            if os.access(send_path, os.R_OK):
+                pass
         else:
             exit(2)
         
-        # SPY PATH
-
-        spy_path=lines[4]
-        if spy_path.lower().startswith("spy_path"):
-            x = spy_path.split("=")
-            spy_path=x[1]
-        if spy_path.endswith("\n"):
-            spy_path=spy_path[:-1]
-        if os.access(spy_path, os.W_OK):
-            pass
-        else:
-            exit(2)
-        # print(server_port)
-        # return server_port, inbox_path
         PORT=server_port
-        INBOX_PATH=inbox_path
-    except:
-        exit(1)
+        INBOX_PATH=send_path
+
 
 
 def send_data(conn , string):
@@ -154,11 +123,11 @@ def send_response(conn , res):
     # data=0
     # return data
 
-def main():
+
+def setup_client_connection():
     global PORT
     global IP
-    get_port_and_path()
-
+    
     """ Staring a TCP socket. """
     server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
@@ -169,37 +138,44 @@ def main():
     """ Server is listening, i.e., server is now waiting for the client to connected. """
     server.listen(1)
 
-    while True:
-        """ Server has accepted the connection from the client. """
-        conn, addr = server.accept()
+
+    conn, addr = server.accept()
+
+    return conn
+
+def main():
+    get_port_and_path()
+    conn = setup_client_connection()
+
 
         # Service redy messagess
-        send_data(conn, "220 Service ready\r\n")
-        check_status_code(conn, "EHLO")
-        send_data(conn, "250 127.0.0.1\r\n")
-        # send_response(conn)
-        check_status_code(conn, "MAIL")
-        send_data(conn, "250 Requested mail action okay completed\r\n")
-        check_status_code(conn, "RCPT")
-        send_data(conn, "250 Requested mail action okay completed\r\n")
-        check_status_code(conn, "RCPT")
-        send_data(conn, "250 Requested mail action okay completed\r\n")
-        check_status_code(conn, "DATA")
-        send_data(conn, "354 Start mail input and <CRLF>.<CRLF>\r\n")
-        check_status_code(conn, "")
-        send_data(conn, "354 Start mail input and <CRLF>.<CRLF>\r\n")
-        check_status_code(conn, "")
-        send_data(conn, "354 Start mail input and <CRLF>.<CRLF>\r\n")
-        check_status_code(conn, "")
-        send_data(conn, "354 Start mail input and <CRLF>.<CRLF>\r\n")
-        check_status_code(conn, "")
-        send_data(conn, "354 Start mail input and <CRLF>.<CRLF>\r\n")
+    send_data(conn, "220 Service ready\r\n")
+    check_status_code(conn, "EHLO")
+    send_data(conn, "250 127.0.0.1\r\n")
+    # send_response(conn)
+    check_status_code(conn, "MAIL")
+    send_data(conn, "250 Requested mail action okay completed\r\n")
+    check_status_code(conn, "RCPT")
+    send_data(conn, "250 Requested mail action okay completed\r\n")
+    check_status_code(conn, "RCPT")
+    send_data(conn, "250 Requested mail action okay completed\r\n")
+    check_status_code(conn, "DATA")
+    print("dataaaaaaaaa")
+    send_data(conn, "354 Start mail input and <CRLF>.<CRLF>\r\n")
+    check_status_code(conn, "")
+    send_data(conn, "354 Start mail input and <CRLF>.<CRLF>\r\n")
+    check_status_code(conn, "")
+    send_data(conn, "354 Start mail input and <CRLF>.<CRLF>\r\n")
+    check_status_code(conn, "")
+    send_data(conn, "354 Start mail input and <CRLF>.<CRLF>\r\n")
+    check_status_code(conn, "")
+    send_data(conn, "354 Start mail input and <CRLF>.<CRLF>\r\n")
 
 
 
-        """ Closing the connection from the client. """
-        conn.close()
-        # print(f"[DISCONNECTED] {addr} disconnected.")
+    """ Closing the connection from the client. """
+    conn.close()
+    # print(f"[DISCONNECTED] {addr} disconnected.")
 
 if __name__ == "__main__":
     main() 
